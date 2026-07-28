e8223f8818bf24971120dc1e9e5f93b6b9a23c34
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-07-28 19:11:11 +0530

    Time: 1 ms (64.33%), Space: 47.3 MB (58.53%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int total= r*c;
-        int [][] arr= new int[r][c];
-        if(mat.length*mat[0].length!= r*c) return mat;
-        for(int k=0;k<total;k++){
-            int oldRow=k/mat[0].length;
-            int oldCol= k%mat[0].length;
-            int newRow= k/c;
-            int newCol=k%c;
-            arr[newRow][newCol]= mat[oldRow][oldCol];
+        int total=r*c;
+        int [][] arr= new int [r][c];
+        if(mat.length * mat[0].length!=total) return mat;
+        for(int i=0;i<total;i++){
+            int oldrow=i/mat[0].length;
+            int oldcol=i%mat[0].length;
+            int newrow=i/arr[0].length;
+            int newcol=i%arr[0].length;
+            arr[newrow][newcol]=mat[oldrow][oldcol];
         }
         return arr;
     }
