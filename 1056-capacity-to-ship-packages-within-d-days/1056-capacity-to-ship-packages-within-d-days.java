0977b96e6dd1867b7825ead2fd237983dc2c8b6d
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-08-02 12:18:51 +0530

    Time: 11 ms (67.18%), Space: 50.2 MB (38.6%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,30 +1,30 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int left=0;
+        int left=Integer.MIN_VALUE;
         int right=0;
+        int ans=0;
         for(int i=0;i<weights.length;i++){
-            left=Math.max(weights[i],left);
+            left=Math.max(left,weights[i]);
             right+=weights[i];
-       }
-       int miny=Integer.MAX_VALUE;
-       while(left<=right){
-        int mid = (left+right)/2;
-        int count=1;
-        int sum=0;
-        for(int i=0;i<weights.length;i++){
-             sum+=weights[i];
-            if(sum>mid){
-                count++;
-                sum=weights[i];
+        }
+        while(left<=right){
+            int mid= left+(right-left)/2;
+            int sum=0;
+            int count=0;
+            for(int i=0;i<weights.length;i++){
+                sum+=weights[i];
+                if(sum>mid){
+                    count++;
+                    sum=weights[i];
+                } 
+            }
+            if(count<days){
+                right=mid-1;
+                ans=mid;
+            }else{
+                left=mid+1;
             }
         }
-        if(count<=days){
-            right=mid-1;
-            miny=mid;
-        }else{
-            left=mid+1;
-        }
-       }
-        return miny;
+        return ans;
     }
 }
