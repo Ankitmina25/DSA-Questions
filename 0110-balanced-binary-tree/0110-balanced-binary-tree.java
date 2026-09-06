e665441920d517b036e09acea590de9cb2ec30eb
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-06 12:34:44 +0530

    Time: 0 ms (100%), Space: 45.3 MB (89.04%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,16 +15,15 @@
  */
 class Solution {
     public int helper(TreeNode root){
-        if(root==null ) return 0;
-
-        int left=helper(root.left);
+        if(root==null) return 0;
+        int left= helper(root.left);
         if(left==-1) return -1;
         int right=helper(root.right);
-        if(right==-1) return -1;
-        if(Math.abs(left-right)>=2) return -1;
+        if(right==-1)return -1;
+         if (Math.abs(left - right) >= 2) return -1;
         return 1+Math.max(left,right);
     }
     public boolean isBalanced(TreeNode root) {
-    return helper(root)!=-1;
+      return helper(root)!=-1;
     }
 }
