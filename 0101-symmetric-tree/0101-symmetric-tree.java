4ea7b7db19baf584d94f365e030d074ec5e19bbf
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-06 11:58:58 +0530

    Time: 0 ms (100%), Space: 43.8 MB (15.64%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,14 +14,14 @@
  * }
  */
 class Solution {
-    public boolean helper(TreeNode left, TreeNode right){
-        if(left == null && right == null) return true;
+    public boolean match(TreeNode left,TreeNode right){
+        if(left==null && right==null) return true;
         if(left==null || right==null) return false;
         if(left.val!=right.val) return false;
-        return helper(left.left,right.right) && helper(left.right,right.left);
+        return match(left.left,right.right) && match(left.right,right.left); 
     }
     public boolean isSymmetric(TreeNode root) {
-        if(root== null) return true;
-      return helper(root.left,root.right);
+        if(root==null) return true;
+        return match(root.left,root.right);
     }
 }
