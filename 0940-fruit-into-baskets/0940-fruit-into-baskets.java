7486e18ab066e7616746a655e9634e1f19c2cea0
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-10 21:37:57 +0530

    Time: 55 ms (42.35%), Space: 70.7 MB (29.38%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -3,15 +3,18 @@ class Solution {
         HashMap<Integer,Integer> map= new HashMap<>();
         int left=0;
         int right=0;
-        int maxy= Integer.MIN_VALUE;
+        int maxy=0;
+        int count=0;
         while(right<fruits.length){
-            while(!map.containsKey(fruits[right]) && map.size()>=2){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size()>2){
                 map.put(fruits[left],map.get(fruits[left])-1);
-                if(map.get(fruits[left])==0) map.remove(fruits[left]);
+                if(map.get(fruits[left])==0){
+                    map.remove(fruits[left]);
+                }
                 left++;
             }
-            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
-            maxy=Math.max(maxy,(right-left)+1);
+            maxy=Math.max((right-left)+1,maxy);
             right++;
         }
         return maxy;
