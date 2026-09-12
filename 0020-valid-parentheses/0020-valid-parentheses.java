809fd54490980c90305528af5d27e63055470ce4
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-12 12:54:22 +0530

    Time: 3 ms (85.97%), Space: 43.4 MB (25.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,15 +3,13 @@ class Solution {
         Stack<Character> st= new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch= s.charAt(i);
-            if(ch=='(' || ch=='[' || ch=='{') st.push(ch);
-            else{
+            if(ch=='(' || ch=='{' || ch=='[') st.push(ch);
+            else {
                 if(st.isEmpty()) return false;
-                if(ch==')' && st.peek()=='(' || ch==']' && st.peek()=='[' || ch=='}' && st.peek()=='{') st.pop();
+                if(ch==')' && st.peek()=='(' || ch=='}'  && st.peek()=='{'|| ch== ']' && st.peek() == '[') st.pop();
                 else return false;
-            }
+            } 
         }
-        if(!st.isEmpty()) return false;
-        return true;
-
+        return st.isEmpty();
     }
 }
