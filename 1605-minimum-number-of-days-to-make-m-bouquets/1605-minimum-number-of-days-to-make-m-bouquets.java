0503c502bba94cde255bfd602c3a521b754d9482
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-12 12:18:47 +0530

    Time: 39 ms (6.39%), Space: 74.9 MB (99.63%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,40 +1,33 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int [] arr= new int[bloomDay.length];
-        int left=Integer.MAX_VALUE;
-        int right=Integer.MIN_VALUE;
-        int ans=0;
-        if(bloomDay.length<(long)m*k) return -1;
+        int arr[]=new int[bloomDay.length];
+        int left=0;
+        int right=0;
+        int ans=-1;
         for(int i=0;i<bloomDay.length;i++){
             left=Math.min(bloomDay[i],left);
             right=Math.max(bloomDay[i],right);
         }
         while(left<=right){
-            int mid= left+(right-left)/2;
-            for(int i=0;i<bloomDay.length;i++){
-                if(bloomDay[i]<=mid){
-                    arr[i]=1;
-                }else{
-                    arr[i]=0;
-                }
+            int mid=(left+right)/2;
+            for(int i=0;i<arr.length;i++){
+                if(bloomDay[i]<=mid) arr[i]=1;
+                else arr[i]=0;
             }
             int count=0;
             int b=0;
             for(int i=0;i<arr.length;i++){
                 if(arr[i]==1) count++;
-                if(count==k){
+                if(count==k) {
                     b++;
                     count=0;
                 }
                 if(arr[i]==0) count=0;
-                
             }
             if(b>=m){
                 ans=mid;
                 right=mid-1;
-            }else{
-                left=mid+1;
-            }
+            }else left=mid+1;
         }
         return ans;
     }
