573961ff5e9b0639f8177b1ab85554fcd0af6baf
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-23 21:13:03 +0530

    Time: 0 ms (100%), Space: 46.6 MB (86.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -14,10 +14,11 @@ public class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
             slow=slow.next;
-            if(fast==slow) return true;
-
+            fast=fast.next.next;
+            if(fast==slow){
+                return true;
+            }
         }
         return false;
     }
