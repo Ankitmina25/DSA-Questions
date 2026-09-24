a1f781072f34cad346728eee3bf7afbefa32bd61
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-24 11:56:41 +0530

    Time: 26 ms (26.31%), Space: 46.6 MB (24.58%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> map= new HashMap<>();
-        HashMap<Character,Integer> map2= new HashMap<>();
+        HashMap<Character,Integer>map1=new HashMap<>();
+        HashMap<Character,Integer>map2=new HashMap<>();
         for(int i=0;i<s1.length();i++){
-            char ch= s1.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            char ch=s1.charAt(i);
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
         }
         int left=0;
         int right=0;
         while(right<s2.length()){
-            char c= s2.charAt(right);
-            map2.put(c,map2.getOrDefault(c,0)+1);
-            if(right-left+1>s1.length()){
-                char cl= s2.charAt(left);
-                map2.put(cl,map2.get(cl)-1);
-                if(map2.get(cl)==0) map2.remove(cl);
-                    left++;
-            } 
-            if((right-left)+1==s1.length()){
-                if(map2.equals(map)) return true;
+            char ch=s2.charAt(right);
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+            if((right-left)+1>s1.length()){
+                char c=s2.charAt(left);
+                map2.put(c,map2.get(c)-1);
+                if(map2.get(c)==0) map2.remove(c);
+                left++;
+            }
+            if(right-left+1==s1.length()){
+                if(map2.equals(map1)) return true;
             }
             right++;
         }
