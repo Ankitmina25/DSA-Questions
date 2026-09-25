81b3046ba7a2219091a6807e449df2b9392c856c
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-25 12:36:25 +0530

    Time: 1 ms (84.01%), Space: 42.5 MB (93.74%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,24 +1,16 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        HashMap<Character,String> map1= new HashMap<>();
-        HashMap<String,Character> map2=new HashMap<>();       
-          String [] words= s.split(" ");
-            if(pattern.length()!=words.length) return false;
-            for(int i=0;i<pattern.length();i++){
-            char ch= pattern.charAt(i);
-            String word= words[i];
-            if(!map1.containsKey(ch) && !map2.containsKey(word)){
-                map1.put(ch,word);
-                map2.put(word,ch);
-            }
-            else if(map1.containsKey(ch) && map2.containsKey(word)) {
-               if (!map1.get(ch).equals(word) || !map2.get(word).equals(ch)){
-                return false;
-               }
-            }
-            else {
-                return false;
-            }
+        String[] words= s.split(" ");
+        HashMap<Character,String> map= new HashMap<>();
+        HashMap<String,Character> rmap=new HashMap<>();
+        if(pattern.length()!=words.length) return false;
+        for(int i=0;i<pattern.length();i++){
+            char ch=pattern.charAt(i);
+            String c=words[i];
+            if(map.containsKey(ch) && !map.get(ch).equals(c)) return false;
+            if(rmap.containsKey(c) && rmap.get(c)!=ch) return false;
+             map.put(ch,c);
+             rmap.put(c,ch);
         }
         return true;
     }
