20522052a4074f714202bab577b9e5d1b3959e93
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-25 10:30:49 +0530

    Time: 2 ms (1.01%), Space: 48.3 MB (69.08%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -3,12 +3,11 @@ class Solution {
         int left=0;
         int right=nums.length-1;
         while(left<=right){
-            int mid= (left+right)/2;
+            int mid=(left+right)-1/2;
             if(nums[mid]==target) return mid;
-            else if( nums[mid]<target) left++;
-            else right--;
+            else if(nums[mid]>target) right=mid-1;
+            else left=mid+1;
         }
         return -1;
-        
     }
 }
