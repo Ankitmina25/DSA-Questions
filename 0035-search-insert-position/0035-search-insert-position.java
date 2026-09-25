6b8d1e33bccd969f1586d0ae34f0e35ad4d61354
DevOp_Ankit <128024619+Ankitmina25@users.noreply.github.com>
2026-09-25 10:40:48 +0530

    Time: 0 ms (100%), Space: 45.1 MB (12.54%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,14 +3,10 @@ class Solution {
         int left=0;
         int right=nums.length-1;
         while(left<=right){
-            int mid=left+(right-left)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]>target) {
-                right=mid-1;
-            }else {
-                left=mid+1;
-            }
+            int mid=(left+right)-1/2;
+            if(nums[mid]==target) return mid;
+            if(target>nums[mid]) left=mid+1;
+            else if(target<nums[mid]) right=mid-1;
         }
         return left;
     }
